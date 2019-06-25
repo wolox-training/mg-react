df74750c6eed1ccaa20de86798f9256b827ea577
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-06-25 18:03:11 -0500

    Now copy function can copy objects

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,15 +1,15 @@
 import { isArray } from './utils';
 
-export function min() {
-  if (arguments[0] === undefined) {
+export function min(...args) {
+  if (args[0] === undefined) {
     return undefined;
   }
-  if (isArray(arguments[0])) {
-    return Math.min(...arguments[0]);
+  if (isArray(args[0])) {
+    return Math.min(...args[0]);
   }
-  return Math.min(...arguments);
+  return Math.min(...args);
 }
 
-export function copy() {
-
+export function copy(args) {
+  return Object.assign({}, args);
 }
