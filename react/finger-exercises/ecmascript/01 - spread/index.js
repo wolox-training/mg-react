39b484295379c30cebce1e64f45bd766578c4466
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-06-25 15:36:33 -0500

    Now min function can return the minimum based on many parameters

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,10 +1,11 @@
 import { isArray } from './utils';
 
-export function min(arg) {
-  if (isArray(arg)) {
-    return Math.min(...arg);
+export function min(...arg) {
+  if (isArray(...arg)) {
+    const list = [...arg];
+    return Math.min(...list[0]);
   }
-  return Math.min(arg);
+  return Math.min(...arg);
 }
 
 export function copy() {
