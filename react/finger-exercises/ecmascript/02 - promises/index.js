a2ffe51d9818f10575df05138de4a0b6d7bb9c2c
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-06-26 16:52:00 -0500

    asyncDelay function also works well

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -16,6 +16,19 @@ export function delay(period) {
   });
 }
 
-export function asyncDelay() {
-
+export function asyncDelay(period) {
+  const startTime = Date.now();
+  const MAX_TIME = 4900;
+  const timePromise = new Promise((resolve, reject) => {
+    setInterval(() => {
+      const endTime = Date.now();
+      if (endTime - startTime > period) {
+        resolve(endTime - startTime);
+      } else if (endTime - startTime > MAX_TIME) {
+        const patient = Error('This time is too much !');
+        reject(patient);
+      }
+    }, 100);
+  });
+  return timePromise;
 }
