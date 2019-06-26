ea56e845766628725864918d7fac9a6758fea47e
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-06-26 16:28:26 -0500

    filterAttribs function does not mutate the argument

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,12 +1,12 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
 export function delay(period) {
-  const now = Date.now();
+  const startTime = Date.now();
   return new Promise((resolve) => {
     setInterval(() => {
-      const time = Date.now();
-      if (time - now > period) {
-        resolve(time - now);
+      const endTime = Date.now();
+      if (endTime - startTime > period) {
+        resolve(endTime - startTime);
       }
     }, 100);
   });
