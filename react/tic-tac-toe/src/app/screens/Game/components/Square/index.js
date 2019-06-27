c84a43634953dbc2102b36598d5d40a55c3dfc96
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-06-27 16:49:57 -0500

    Passing Data Through Props

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-const Square = () => (
+const Square = props => (
   <button type="button" className={styles.square}>
-    {/* TODO */}
+    {props.value}
   </button>
 );
+
+Square.propTypes = {
+  value: PropTypes.number
+};
 
 export default Square;
