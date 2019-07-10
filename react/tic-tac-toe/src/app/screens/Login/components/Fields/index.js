98573e85256a29ea69a75c63708f8c7a186c9d04
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-07-10 15:19:40 -0500

    developing login layout

---
+++ react/tic-tac-toe/src/app/screens/Login/components/Fields/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import cx from 'classnames';
+
+class customInput extends Component {
+  getValidityClassName = meta => {
+    const { active, touched, invalid, valid, asyncValidating } = meta;
+
+    if (active) {
+      return 'active';
+    }
+    if (touched && invalid) {
+      return 'invalid';
+    }
+    if (touched && valid) {
+      return 'valid';
+    }
+    if (asyncValidating) {
+      return 'async-validating';
+    }
+    return '';
+  };
+
+  render() {
+    const { label, input, type, meta } = this.props;
+    const { dirty, error, touched, active } = meta;
+    return (
+      <div
+        className={cx(
+          'custom-input-container',
+          { 'flex-row-reverse': type === 'checkbox' },
+          { dirty },
+          this.getValidityClassName(meta)
+        )}
+      >
+        <input {...input} type={type} />
+        <label>{label}</label>
+        {error && touched && !active && <div className="feedback-text error-text">{error}</div>}
+      </div>
+    );
+  }
+}
+
+export default customInput;
