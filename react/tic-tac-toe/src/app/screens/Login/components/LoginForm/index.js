496df9dadd528e56e10c45301cb7870e9fbcba09
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-07-12 11:53:17 -0500

    Ussing arrow function instead class

--- react/tic-tac-toe/src/app/screens/Login/components/LoginForm/index.js
+++ react/tic-tac-toe/src/app/screens/Login/components/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { func } from 'prop-types';
 
@@ -7,37 +7,25 @@ import { required, minLength, isMail } from '../Validation';
 
 import styles from './styles.modules.scss';
 
-class LoginForm extends Component {
-  handleSubmit = () => {
-    const { onSubmit } = this.props;
-    onSubmit();
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={styles.form}>
-        <Field
-          name="useremail"
-          component={customInput}
-          type="text"
-          label="Mail"
-          validate={[required, isMail]}
-        />
-        <Field
-          name="password"
-          component={customInput}
-          type="password"
-          label="Password"
-          validate={[required, minLength]}
-        />
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-}
+const LoginForm = props => {
+  const { handleSubmit } = props;
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <Field name="email" component={customInput} type="text" label="Mail" validate={[required, isMail]} />
+      <Field
+        name="password"
+        component={customInput}
+        type="password"
+        label="Password"
+        validate={[required, minLength]}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
 
 LoginForm.propTypes = {
-  onSubmit: func.isRequired
+  handleSubmit: func.isRequired
 };
 
 export default reduxForm({
