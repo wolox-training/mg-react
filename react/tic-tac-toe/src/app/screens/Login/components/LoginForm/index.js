66ddf41586955f8deced89a04e6dd37f4f2a3933
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-07-12 11:45:39 -0500

    Connected successfully to api

--- react/tic-tac-toe/src/app/screens/Login/components/LoginForm/index.js
+++ react/tic-tac-toe/src/app/screens/Login/components/LoginForm/index.js
@@ -10,7 +10,7 @@ import './styles.modules.scss';
 const LoginForm = props => {
   const { handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit} className="login">
+    <form onSubmit={handleSubmit} className={styles.form}>
       <Field name="email" component={customInput} type="text" label="Mail" validate={[required, isMail]} />
       <Field
         name="password"
