2656357913c0ae7eb6ad7f2e9e13f2ce4d9ada84
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-07-17 11:23:30 -0500

    using a functional component instead a class

--- react/tic-tac-toe/src/app/screens/Login/index.js
+++ react/tic-tac-toe/src/app/screens/Login/index.js
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React, { Component } from 'react';
->>>>>>> code refactoring, Coding submit function in a superior component
 import { func } from 'prop-types';
 
 import LoginForm from './components/LoginForm';
 
-<<<<<<< HEAD
 function Login({ onSubmit }) {
   return <LoginForm onSubmit={onSubmit} />;
-=======
-class Login extends Component {
-  handleSubmit = values => {
-    const { onSubmit } = this.props;
-    onSubmit(values);
-  };
-
-  render() {
-    return <LoginForm onSubmit={this.handleSubmit} />;
-  }
->>>>>>> code refactoring, Coding submit function in a superior component
 }
 
 Login.propTypes = {
