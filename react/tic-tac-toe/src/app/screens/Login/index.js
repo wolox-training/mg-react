fa69e1fee14fad0a4080ddfa94e671961bcf12b6
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-07-18 12:07:28 -0500

    updating TODO comment

--- react/tic-tac-toe/src/app/screens/Login/index.js
+++ react/tic-tac-toe/src/app/screens/Login/index.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import LoginForm from './components/LoginForm';
 
 class Login extends Component {
-  // TODO handleSubmit = () => {};
+  handleSubmit = () => {
+    // TODO
+  };
 
   render() {
     return <LoginForm onSubmit={this.handleSubmit} />;
