dc173a1c764e3c84fbfb9bdb2b17d66d1c9176f5
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2019-07-26 12:10:56 -0500

    Cleaning the code, erasing unnecessary components

--- react/tic-tac-toe/src/app/components/AppRouter/components/AuthInfo/index.js
+++ react/tic-tac-toe/src/app/components/AppRouter/components/AuthInfo/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { bool, func } from 'prop-types';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChessBoard, faGamepad } from '@fortawesome/free-solid-svg-icons';
 
 import { WELCOME_MSJ, SINGOUT_MSJ, LOGINFAIL_MSJ, PODIUM_LINK, PLAY_LINK } from '~constants';
 
@@ -11,24 +9,24 @@ import styles from './styles.module.scss';
 function AuthInfo({ isAuth, islogged, onClick }) {
   if (islogged) {
     return isAuth ? (
-      <ul className={styles.container}>
-        <h5>{WELCOME_MSJ}</h5>
-        <li>
-          <Link className={styles.link} to="/game">
-            <FontAwesomeIcon icon={faChessBoard} />
-            {PLAY_LINK}
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.link} to="/podium">
-            <FontAwesomeIcon icon={faGamepad} />
-            {PODIUM_LINK}
-          </Link>
-        </li>
-        <button type="button" onClick={onClick} className={styles.button}>
+      <nav className={styles.navbar}>
+        {WELCOME_MSJ}
+        <Link className={styles.navLink} to="/game">
+          <span role="img" aria-label="controller">
+            🎮
+          </span>
+          {PLAY_LINK}
+        </Link>
+        <Link className={styles.navLink} to="/podium">
+          <span role="img" aria-label="badge">
+            🎖
+          </span>
+          {PODIUM_LINK}
+        </Link>
+        <button type="button" onClick={onClick} className={styles.navButton}>
           {SINGOUT_MSJ}
         </button>
-      </ul>
+      </nav>
     ) : (
       <div className={styles.errorContainer}>
         <p className={styles.error}>{LOGINFAIL_MSJ}</p>
--- react/tic-tac-toe/src/app/screens/Podium/index.js
+++ react/tic-tac-toe/src/app/screens/Podium/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Spinner from 'react-spinkit';
 
-import Matches from './components/Matches';
+import getMatches from '~services/MatchesService';
+
 import styles from './styles.module.scss';
 
-function Podium() {
-  return (
-    <div className={styles.container}>
-      <Matches />
-    </div>
-  );
+class Podium extends Component {
+  state = {
+    matches: null,
+    loading: true
+  };
+
+  loadMatches = async () => {
+    const matches = await getMatches();
+    this.setState({ loading: false, matches });
+  };
+
+  componentDidMount() {
+    this.loadMatches();
+  }
+
+  render() {
+    const { matches, loading } = this.state;
+    return (
+      <div className={styles.container}>
+        {loading ? (
+          <Spinner name="ball-scale-ripple" color="green" />
+        ) : (
+          <ul className={styles.matches}>
+            <li>
+              <span role="img" aria-label="coup">
+                🏆
+              </span>
+              <h1>Podium</h1>
+              <span role="img" aria-label="coup">
+                🏆
+              </span>
+            </li>
+            {matches.map(({ createdAt, player_one: playerOne, player_two: playerTwo, winner }) => (
+              <li key={createdAt}>
+                {playerOne} v {playerTwo}: {winner}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Podium;
